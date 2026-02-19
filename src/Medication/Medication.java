3acce82d1eb3414638e562b2d690301d805d92b6
Justin Seaward <jut_seaward@yahoo.com>
2026-02-19 20:10:40 -0330

    added constructor methods for Medication class

--- src/Medication/Medication.java
+++ src/Medication/Medication.java
@@ -5,7 +5,25 @@ import java.util.Date;
 public class Medication {
     private int id;
     private String name;
-    private double dose;
-    private int quanityInStock;
+    private String dose;
+    private int quantityInStock;
     private Date expiryDate;
+
+    public Medication(){
+    }
+
+    public Medication(int id, String name, String dose, Date expiryDate){
+        this.id = id;
+        this.name = name;
+        this.dose = dose;
+        this.expiryDate = expiryDate;
+    }
+
+    public Medication(int id, String name, String dose, int quantityInStock, Date expiryDate){
+        this.id = id;
+        this.name = name;
+        this.dose = dose;
+        this.quantityInStock = quantityInStock;
+        this.expiryDate = expiryDate;
+    }
 }
