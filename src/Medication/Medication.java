9ffd82de907e41d4c978625d13aed2c6dea840d8
Justin Seaward <jut_seaward@yahoo.com>
2026-02-19 19:57:10 -0330

    created Medication clasee with instance variables, created Prescripition class with instance variables

---
+++ src/Medication/Medication.java
@@ -0,0 +1,11 @@
+package Medication;
+
+import java.util.Date;
+
+public class Medication {
+    private int id;
+    private String name;
+    private double dose;
+    private int quanityInStock;
+    private Date expiryDate;
+}
---
+++ src/Medication/Prescription.java
@@ -0,0 +1,10 @@
+package Medication;
+
+public class Prescription {
+    private int id;
+    private Person doctor;
+    private Person patient;
+    private Medication medication;
+    private Prescription expiryDate;
+}
+
