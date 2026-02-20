4bb69a49c50d6cdabd471bbe2e6e11a3e2f65ac6
Chris <98862485+Britten66@users.noreply.github.com>
2026-02-20 09:16:01 -0400

    adding Person Patient Doctor classes under Pharmacy package, commented out pending portion of code needing to be linked

---
+++ src/pharmacy/Doctor.java
@@ -0,0 +1,24 @@
+package pharmacy;
+
+// Team:           Justin Seaward, Christopher Britten
+// Date:           February 20, 2026
+// Description:    Pharmacy midterm semester 2
+// Details:        [Insert extended description here]
+// Time Allocated: 2 weeks
+
+
+public class Doctor extends Person{
+
+    public Doctor(int id, String name, int age, String phoneNumber){
+        super(id, name, age, phoneNumber);
+    }
+
+    // getters here Doctor will reutrn patients and speciilaztion
+
+
+
+    //setters here same thing return nothing and use .this
+
+
+    // tostring "" + "" finish off
+}
---
+++ src/pharmacy/Person.java
@@ -0,0 +1,64 @@
+package pharmacy;
+
+// Team:           Justin Seaward, Christopher Britten
+// Date:           February 20, 2026
+// Description:    Pharmacy midterm semester 2
+// Details:        [Insert extended description here]
+// Time Allocated: 2 weeks
+
+//imports here
+
+public class Person {
+
+    // Person will have id ,name and age along with phone num
+
+    private int id;
+    private String name;
+    private int age;
+    private String phoneNumber;
+
+
+// here is the constructor ..it is parameterized
+
+
+    // when the variable getts called the constructor will assign the variable to the object created
+    public Person(int id, String name, int age, String phoneNumber){
+
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+
+
+    }
+
+    //Getters here .. part of using encapsulation is using getter to access the private variables
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName(){
+        return name;
+
+    }
+
+    public int getAge() {
+        return age;
+
+
+    }
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+
+    // setters will change the private varibale when it is outside of the class
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String toString(){
+        return "ID: " + id + ", Name: " + name + ", Age: " + age + "Phone Number: " + phoneNumber;
+    }
+}
