6435b9b0d1a2d1d15eeafd5e221585bdd0f205d1
Justin Seaward <jut_seaward@yahoo.com>
2026-02-21 13:21:35 -0330

    added getters, setters and toString method

--- src/Medication/Prescription.java
+++ src/Medication/Prescription.java
@@ -1,7 +1,7 @@
 package Medication;
 import pharmacy.Person;
 
-public class Prescription {
+public class Prescription{
     private int id;
     private Person doctor;
     private Person patient;
@@ -18,6 +18,49 @@ public class Prescription {
             this.expiryDate = expiryDate;
         }
 
-        
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Person getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Person doctor) {
+        this.doctor = doctor;
+    }
+
+    public Person getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Person patient) {
+        this.patient = patient;
+    }
+
+    public Medication getMedication() {
+        return medication;
+    }
+
+    public void setMedication(Medication medication) {
+        this.medication = medication;
+    }
+
+    public Medication getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Medication expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public String toString() {
+        return "Prescription information - " + "ID:" + this.id + " Doctor: " + this.doctor + " Patient: " + this.patient
+                + "Medication: " + this.medication + "Medication expiry date: " + this.expiryDate;
+    }
 }
 
