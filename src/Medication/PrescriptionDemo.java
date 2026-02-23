205ed15bf7ce0d0cf846dfffa6130ce44ad7b6a8
Justin Seaward <jut_seaward@yahoo.com>
2026-02-22 21:40:32 -0330

    deleted import for doctor and patient class and renamed Demo class to PrescritpionDemo

--- src/Medication/PrescriptionDemo.java
+++ src/Medication/PrescriptionDemo.java
@@ -1,14 +1,14 @@
 package Medication;
 
-import pharmacy.Doctor;
-import pharmacy.Patient;
+//import Doctor;
+//import Patient;
 
 import java.time.LocalDate;
 
-public class Demo {
+public class PrescriptionDemo {
 
     public static void main(String[] args){
-        Prescription p1 = new Prescription(101, new Doctor(34,"Scamper Scamper", 69, "7093456543"),
+        Prescription p1 = new Prescription(101, new Doctor(34,"Scamper Scamper", 69, "7093456543","dogs"),
                 new Patient(32, "Steven seagull",32, "7098766789"),
                 new Medication(432,"oxy", "One pill in the last evening", LocalDate.of(2026,6,6)),
                 LocalDate.of(2027,7,7));
