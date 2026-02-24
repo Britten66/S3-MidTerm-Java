e7d1aee5c4c8d1e262aea7f7188f9f54302644b3
Chris <98862485+Britten66@users.noreply.github.com>
2026-02-24 14:48:34 -0400

    complete MedicationTrackingSystem people methods

--- src/Medication/MedicationTrackingSystem.java
+++ src/Medication/MedicationTrackingSystem.java
@@ -8,14 +8,14 @@ public class MedicationTrackingSystem {
 
     // here are the private variables that link to the classes made
 
-private List<Patient> patients;
-private List<Doctor> doctors;
-private List<Medication> medications;
+    private List<Patient> patients;
+    private List<Doctor> doctors;
+    private List<Medication> medications;
 
 
-// creating the default constructor for the medication tracking system here
+   // creating the default constructor for the medication tracking system here
 
-    public MedicationTrackingSystem(){
+    public MedicationTrackingSystem() {
 
         this.patients = new ArrayList<>();
         this.doctors = new ArrayList<>();
@@ -27,31 +27,30 @@ private List<Medication> medications;
 
     // adding a patient to doctor - we did this in the test class driving tests forward as develeopement progresses
 
-    public void addPatient(Patient patient){
+    public void addPatient(Patient patient) {
         patients.add(patient);
-        System.out.println("Patient " + patient.getName() + " added" );
+        System.out.println("Patient " + patient.getName() + " added");
 
     }
 
-//    public void deletePatient(Patient patient){
-//        patients.remove(patient);
-//        System.out.println(("Patient " + patient.getName()) + " removed");
-//
-//    }
 
-    // we can only call .remove if java see's an object created
-    // since we will be using scanner and dont have anything set to a value here is a alt method with a loop like the test.
+    //       public void deletePatient(Patient patient){
+    //        patients.remove(patient);
+    //        System.out.println(("Patient " + patient.getName()) + " removed");
+
+    //      we can only call .remove if java see's an object created
+    //       since we will be using scanner and dont have anything set to a value here is a alt method with a loop like the test.
 
     // here is the remove patient as a loop '
 
 
-    public void deletePatient(int id){
+    public void deletePatient(int id) {
 
 
         // this is looping through  patients
-        for(int i = 0; i < patients.size(); i++){
+        for (int i = 0; i < patients.size(); i++) {
 
-            if(patients.get(i).getId() == id){
+            if (patients.get(i).getId() == id) {
                 // remove at the position found
 
                 patients.remove(i);
@@ -65,40 +64,173 @@ private List<Medication> medications;
         }
 
 
-            // this will get printed if nothing found
+        // this will get printed if nothing found
         System.out.println("No Patient Under That Name Found");
+
 
     }
 
-    public
 
-
-
-
-
-
-    public void updatePatient(int id, String newName, int newAge, String newPhone){
+    public void updatePatient(int id, String newName, int newAge, String newPhone) {
 
 
         // for loop searching through the patients again
 
-        for(int i = 0; i < patients.size(); i++){
+        for (int i = 0; i < patients.size(); i++) {
 
             if (patients.get(i).getId() == id) {
                 patients.get(i).setName(newName);
                 patients.get(i).setAge(newAge);
                 patients.get(i).setNewPhone(newPhone);
+
                 System.out.println("Patient Updated");
                 return;
+            }
 
+        }
+        System.out.println("Patient Not Found!");
+
+    }
+
+    // adding doctor ..similar to add patient this will include the doctor.add part
+
+    public void addDoctor(Doctor doctor) {
+        doctors.add(doctor);
+        System.out.println("Doctor: " + doctor.getName() + " added ");
+
+
+    }
+
+
+    // removing a doctor will be the same as patient as .remove wont be applicable in this case
+
+    public void deleteDoctor(int id) {
+
+        for (i = 0; i < doctors.size(); i++) {
+            if (doctors.get(i).getId() == id) {
+
+                doctors.remove(i);
+                System.out.println("Doctor Removed");
+
+            }
+
+
+        }
+    }
+
+    // update doctor
+    // similar to last it will include for loop but go through each and prompt for an update
+
+    public void updateDoctor(int id, String newName, int newAge, String newPhone) {
+
+        for (int i = 0; i < doctors.size(); i++) {
+            if (doctors.get(i).getId() == id) {
+                doctors.get(i).setName(newName);
+                doctors.get(i).setAge(newAge);
+                doctors.get(i).setNewPhone(newPhone);
+
+                System.out.println("Doctor Updated");
+                return;
             }
 
         }
 
-        System.out.println("Patient Not Found!");
 
+        System.out.println("No Doctor Found");
 
     }
+
+    //searching doctor by name will be same search as before
+
+    public void searchDoctor(String name){
+
+        for(int i = 0; i < doctors.size(); i++){
+            if (doctors.get(i).getName().equalsIgnoreCase(name)){
+
+            System.out.println("Doctor found: " + doctors.get(i));
+
+            return;
+
+        }
+            System.out.println("NO doctor found");
+    }
+
+    // adding a patient to the doctor
+    // this will find both id's and link the 2
+
+    //they will both will need to be set to null starting off
+
+        public void addPatientToDoctor(int patientId, int doctorId){
+
+            // setting found variables to null
+
+            Patient foundPatient = null;
+            Doctor foundDoctor = null;
+
+                // looping through patients for id
+
+            for(int i = 0; i < patients.size(); i++){
+
+                if(patients.get(i).getId() == patientId){
+                    foundPatient = patients.get(i);
+                }
+
+            }
+
+            // looping through doctors for id
+
+            for(int i = 0; i < doctors.size(); i++){
+
+                if(doctors.get(i).getId() == doctorId){
+                    foundDoctor = doctors.get(i);
+                }
+            }
+
+            if(foundPatient != null && foundDoctor != null){
+                foundDoctor.addPatient((foundPatient));
+
+                System.out.println("Patient added to Doctor");
+            } else {
+                System.out.println("Patient or Doctor Not Found");
+            }
+
+                    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
