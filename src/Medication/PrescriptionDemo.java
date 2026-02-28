6822dc63bc7ffcb856e8a59e211a83a3f4cb480d
Justin Seaward <jut_seaward@yahoo.com>
2026-02-28 10:29:20 -0330

    Added new prescriptions to the Demo

--- src/Medication/PrescriptionDemo.java
+++ src/Medication/PrescriptionDemo.java
@@ -11,6 +11,24 @@ public class PrescriptionDemo {
                 new Medication(432,"Oxycodo", "One pill before bed", LocalDate.of(2026,6,6)),
                 LocalDate.of(2026,2,10).plusYears(1));
 
+        Prescription p2 = new Prescription(201,
+                new Doctor(30, "Bat Man", 37, "7091233456", "BadGuys"),
+                new Patient(43, "Bender", 20, "1011011010"),
+                new Medication(23, "WD-40", "Three times a day, on all joints", LocalDate.of(2027,2,28)),
+                LocalDate.of(2026,2,10).plusYears(1));
+
+        Prescription p3 = new Prescription(301,
+                new Doctor(55, "Dr. Dolittle", 42, "5550123456", "Animals"),
+                new Patient(99, "Scooby Doo", 7, "8007266291"),
+                new Medication(77, "Scooby Snacks", "One per mystery solved", LocalDate.of(2028, 12, 25)),
+                LocalDate.of(2026, 2, 28).plusYears(1));
+
+        Prescription p4 = new Prescription(401,
+                new Doctor(90, "Doc Brown", 65, "1211985195", "Time Travel"),
+                new Patient(12, "Marty McFly", 17, "5558882311"),
+                new Medication(88, "Plutonium", "Handle with care at 88mph", LocalDate.of(1955, 11, 5)),
+                LocalDate.of(2026, 2, 28).plusYears(1))
+
         System.out.println("Doctor - " + p1.getDoctor());
         System.out.println("Medication - " + p1.getMedication());
         System.out.println("Prescription Expiry Date - " + p1.getPrescriptionExpiry());
