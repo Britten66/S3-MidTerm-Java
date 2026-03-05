f1114add379e23fb8f79f66556f6f5f5501bc5d0
Chris <98862485+Britten66@users.noreply.github.com>
2026-03-05 12:07:22 -0400

    final update fixed dupes and error causing menu to crash

--- src/Medication/Patient.java
+++ src/Medication/Patient.java
@@ -1,123 +1,65 @@
 package Medication;
 
+
 // Team:           Justin Seaward, Christopher Britten
 // Date:           February 20, 2026
 // Description:    Pharmacy midterm semester 3
 // Details:        Patient class - extends Person, holds medications and prescriptions
 // Time Allocated: 2 weeks
 
+import java.util.ArrayList;
+import java.util.List;
 
-// this extends Person meaning that Patient will inherit all of Persons attributres , id name etc
+// this extends Person meaning that Patient will inherit all of Person's attributes, id name etc
+// extending Person class ( this is showcasing inheritance )
 
-    import java.util.ArrayList;
-    import java.util.List;
+public class Patient extends Person {
 
-
-    // extending Person class ( this is showcasing inheritance )
-
-    public class Patient extends Person {
-
-        // assigning list to other class
-
-        private List<Medication> medications;
-        private List<Prescription> prescriptions;
-
+    // assigning list to other class
+    private List<Medication> medications;
+    private List<Prescription> prescriptions;
 
     public Patient(int id, String name, int age, String phoneNumber) {
-
         super(id, name, age, phoneNumber);
         this.medications = new ArrayList<>();
         this.prescriptions = new ArrayList<>();
     }
 
+    // getters here - going to be getting private variables from Justin's classes
+    // otherwise this is also commonly known as encapsulation
+    // returning them outside of the private class makes them accessible
 
-        // getters here - going to be getting private variables from Justin's classes
-        // otherwise this is also commonly known as encapsulation
-        // returning them outside of the private class makes them accessable
-
-
-
-        public List<Medication> getMedications(){
-
-
-
-
+    public List<Medication> getMedications() {
         return medications;
-        }
+    }
 
-
-        public List<Prescription> getPrescriptions(){
-
-
-
-
+    public List<Prescription> getPrescriptions() {
         return prescriptions;
-        }
+    }
 
+    // setters are here
+    // this can be referred to as data hiding
+    // setters utilize encapsulation to show the data outside the private class
 
-        //setters are here this is
-
-        // this can be refered to as data hiding, if we are discussing with somone non technical
-        // setters utilize encapulation and abstration to show the data outside the private class
-
-
-
-        public void setMedications(List<Medication> medications) {
-
+    public void setMedications(List<Medication> medications) {
         this.medications = medications;
+    }
 
-
-        }
-
-        public void setPrescriptions(List<Prescription> prescriptions){
-
+    public void setPrescriptions(List<Prescription> prescriptions) {
         this.prescriptions = prescriptions;
+    }
 
-        }
+    // medications need to be registered one at a time as per requirement
 
-        // this medications need to be registared one at a time as per requirment
-        // this needs a helper to handle any errors that may come up
-
-        public void addMedication(Medication medication){
-
+    public void addMedication(Medication medication) {
         this.medications.add(medication);
-        }
+    }
 
-        public void addPrescription(Prescription prescription){
-
+    public void addPrescription(Prescription prescription) {
         this.prescriptions.add(prescription);
-        }
+    }
 
-
-
-
-
-
-        public String toString(){
-        return super.toString() + ", Medications " + medications.size() + "< Prescriptions " + prescriptions.size();
-
-
-
-
+    public String toString() {
+        return super.toString() + ", Medications: " + medications.size() + ", Prescriptions: " + prescriptions.size();
     }
 }
-
-
-
-//some messy notes im leaving for showing thought process
-
-
-//super sends to parent to store as child cannot store it
-
-
-// getters for medicaiton and perscription will go here
-// remember to return medication and perscriptiuon
-
-// setters will be below
-//same thing but using .this
-//and return nothing .. void
-
-
-
-
-// rememver to print out returning super
